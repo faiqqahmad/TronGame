6c9f5352569f963717b12ae78b9f996d07bb4675
Faiq Ahmad <faiqahmadpk@gmail.com>
2023-08-31 19:13:33 -0400

    Saving project, work done on buttons, button styling, and button placement

--- TronFrontEnd/src/LandingPage.jsx
+++ TronFrontEnd/src/LandingPage.jsx
@@ -1,8 +1,61 @@
 import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
+import '../css/LandingPage.css'
+import { flexbox } from '@mui/system'
+
+
 
 export default function LandingPage() {
+
     return <>
-    {/* <Button variant="contained">TESTING</Button> */}
-    <Box></Box>
+    <Box sx={{ 
+        backgroundColor: 'black',
+        backgroundImage: 'url(../assets/landingPageBackgroundImage.jpg)',
+        width: '100%',
+        height: '100%',
+        display: "flex",
+        justifyContent: "right",
+        alignItems: "flex-end",
+        minHeight:"100vh",
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+    }} >
+        <Box id="buttonList" sx= {{
+            mb: '100px',
+            display: 'flex',
+            flexDirection: 'column'
+            
+        }}>
+            
+            <Button className="buttons" variant="contained" sx= {{
+
+                marginRight: "10vw",
+                mt: "10px",
+                width: "200px",
+                height: "100px"
+
+            }}>TESTING</Button>
+        
+        
+            <Button className="buttons" variant="contained" sx= {{
+
+                marginRight: "10vw",
+                mt: "10px"
+
+            }}>TESTING</Button>
+        
+        
+            <Button className="buttons" variant="contained" sx= {{
+
+                marginRight: "10vw",
+                mt: "10px"
+
+            }}>TESTING</Button>
+            
+        </Box>
+        
+    </Box>
+    
     </>
 }
