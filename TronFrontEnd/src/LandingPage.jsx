9c179992ca780131505eb67ffb22dafc5e390763
Faiq Ahmad <faiqahmadpk@gmail.com>
2023-08-31 10:46:16 -0400

    Started the landing page

---
+++ TronFrontEnd/src/LandingPage.jsx
@@ -0,0 +1,8 @@
+import Button from '@mui/material/Button'
+
+export default function LandingPage() {
+    return <>
+    {/* <Button variant="contained">TESTING</Button> */}
+    <Box></Box>
+    </>
+}
