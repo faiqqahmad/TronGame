3f4f0fd5e63d834cd4efaf1dcad3557cd7703e33
Faiq Ahmad <faiqahmadpk@gmail.com>
2023-09-01 17:36:54 -0400

    added sign up and sign in templates

--- TronFrontEnd/src/App.jsx
+++ TronFrontEnd/src/App.jsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "@emotion/react"
 import LandingPage from "./LandingPage"
+import SignIn from "./SignIn"
+import SignUp from "./SignUp"
 
 
 
 export default function App() {
   return <>
-    <LandingPage></LandingPage>
+    <SignUp></SignUp>
   </>
 }
