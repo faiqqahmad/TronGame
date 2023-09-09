a591e7b18311ad5763375172594f8caf6827b914
Faiq Ahmad <faiqahmadpk@gmail.com>
2023-09-09 06:57:31 -0400

    reorganized file structure into client and server, the client side includes the database

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from "@emotion/react"
+import LandingPage from "./LandingPage"
+import PlayScreenPage from "./PlayScreenPage"
+import SignIn from "./SignIn"
+import SignUp from "./SignUp"
+
+
+
+export default function App() {
+  return <>
+    <SignIn></SignIn>
+  </>
+}
