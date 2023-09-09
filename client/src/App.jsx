f70fcebae227a372bb75795369111fd989d5a5ac
Faiq Ahmad <faiqahmadpk@gmail.com>
2023-09-09 13:57:46 -0400

    started on websockets, got basic express server working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,36 @@ import LandingPage from "./LandingPage"
 import PlayScreenPage from "./PlayScreenPage"
 import SignIn from "./SignIn"
 import SignUp from "./SignUp"
-
+import React, { useEffect, useState } from 'react'
 
 
 export default function App() {
-  return <>
-    <SignIn></SignIn>
-  </>
+
+
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/api",  {
+      header: {
+        'If-None-Match': "*",
+        'Cache-Control': 'no-cache'
+      }
+    }).then(
+      response => response.json()
+    ).then(
+      data => { 
+        setBackendData(data)
+      }
+    )
+  }, [])
+
+  return <div>
+    {( typeof backendData.users == 'undefined' ? (
+      <p>Loading...</p>
+    ) : (
+      backendData.users.map((user,i) =>  (
+        <p key={i}> {user} </p>
+      ))
+    ))}
+  </div>
 }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,25 @@
 const express = require('express')
+const cors = require('cors')
+const WebSocket = require('ws')
 
 const app = express()
+
+app.use(cors())
+
+// const wss = new WebSocket.Server(app)
+
+// wss.on('connection', function connection(ws) { // this is for when a new client connects to the server
+//     console.log('A new client has connected')//
+//     ws.send('Welcome new client!');//
+
+//     ws.on('message', function message(data) { // this is for when the server recieves a new message from the client
+//       console.log('received: %s', data);
+//       ws.send("Got your message its: " + data)
+//     });
+  
+    
+//   }); //   
+  
 
 app.get("/api", (req, res) => {
     res.json({ "users" : ["userOne", "userTwo", "userThree"]})
