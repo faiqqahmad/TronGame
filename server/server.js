78d579b3495bdc8a425e9c8d70aad2f59858afe9
Faiq Ahmad <faiqahmadpk@gmail.com>
2023-09-10 09:55:13 -0400

    save pointer for websocket work, got communication to work between two different users

--- server/server.js
+++ server/server.js
@@ -1,28 +1,31 @@
 const express = require('express')
-const cors = require('cors')
-const WebSocket = require('ws')
-
 const app = express()
+const cors = require('cors')
+// app.use(cors())
+const server = require('http').createServer(app)
+const WebSocket = require('ws')
+const { WebSocketServer } = require('ws')
 
-app.use(cors())
 
-// const wss = new WebSocket.Server(app)
+const wss = new WebSocket.Server( {server: server})
+const w11 = new WebSocketServer()
 
-// wss.on('connection', function connection(ws) { // this is for when a new client connects to the server
-//     console.log('A new client has connected')//
-//     ws.send('Welcome new client!');//
 
-//     ws.on('message', function message(data) { // this is for when the server recieves a new message from the client
-//       console.log('received: %s', data);
-//       ws.send("Got your message its: " + data)
-//     });
+wss.on('connection', function connection(ws) { // this is for when a new client connects to the server
+    console.log('A new client has connected')//
+    ws.send('Welcome new client!');//
+
+wss.on('message', function message(data) { // this is for when the server recieves a new message from the client
+    console.log('received: %s', data);
+    wss.send("Got your message its: " + data)
+    });
   
     
-//   }); //   
+  }); //   
   
 
 app.get("/api", (req, res) => {
     res.json({ "users" : ["userOne", "userTwo", "userThree"]})
 })
 
-app.listen(4000, () => {console.log("Server started on port 4000")})
+app.listen(3000, () => {console.log("Server started on port 3000")})
